c69b65b4bbf783f5a968b3cbc14c41b5fffe5602
JanaSaker <janasakr2000@gmail.com>
2023-09-25 12:29:15 +0300

    step 3

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
+app.listen (3000);
 
 // Define a route that responds with "OK" for all incoming requests.
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
-app.listen (3000);
+// Define a route for /test
+app.get('/test', (req, res) => {
+  res.status(200).json({ status: 200, message: 'ok' });
+});
 
+// Define a route for /time
+app.get('/time', (req, res) => {
+  const hours = new Date().getHours(); 
+  const minutes = new Date().getMinutes(); 
+  const Time = `${hours}:${minutes}`;
+  res.status(200).json( {status:200, message: Time });
+});
 
-// console.log(`Starting Express server on port `);
-// console.log('jana');
