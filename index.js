f824684b518d596b47aa54ee8ae012218bc7755f
JanaSaker <janasakr2000@gmail.com>
2023-09-27 22:11:22 +0300

    step 4

--- index.js
+++ index.js
@@ -20,3 +20,21 @@ app.get('/time', (req, res) => {
   res.status(200).json( {status:200, message: Time });
 });
 
+app.get('/hello/:id?', (req, res) => {
+  const id = req.params.id;
+  if (!id){
+    res.status(200).json({ status: 200, message: `Hello You` });
+
+  }else{
+  res.status(200).json({ status: 200, message: `Hello ${id}` });}
+});
+
+app.get('/search', (req, res) => {
+  const searchQ = req.query.s;
+
+  if (searchQ) {
+    res.status(200).json({ status: 200, message: 'ok', data: searchQ });
+  } else {
+    res.status(500).json({ status: 500, error: true, message: 'You have to provide a search' });
+  }
+});
