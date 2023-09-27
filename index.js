64237928f12daf9e02a37a78d7ee86c2a84b0f5a
JanaSaker <janasakr2000@gmail.com>
2023-09-28 01:05:32 +0300

    step 12

--- index.js
+++ index.js
@@ -1,152 +1,107 @@
 const express = require('express');
 const app = express();
-app.listen (3000);
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  rating: Number,
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
 app.use(express.json());
 
-
-// Define a route that responds with "OK" for all incoming requests.
-app.get('/', (req, res) => {
-  res.send('OK');
-});
-
-// Define a route for /test
-app.get('/test', (req, res) => {
-  res.status(200).json({ status: 200, message: 'ok' });
-});
-
-// Define a route for /time
-app.get('/time', (req, res) => {
-  const hours = new Date().getHours(); 
-  const minutes = new Date().getMinutes(); 
-  const Time = `${hours}:${minutes}`;
-  res.status(200).json( {status:200, message: Time });
-});
-
-app.get('/hello/:id?', (req, res) => {
-  const id = req.params.id;
-  if (!id){
-    res.status(200).json({ status: 200, message: `Hello You` });
-
-  }else{
-  res.status(200).json({ status: 200, message: `Hello ${id}` });}
-});
-
-app.get('/search', (req, res) => {
-  const searchQ = req.query.s;
-
-  if (searchQ) {
-    res.status(200).json({ status: 200, message: 'ok', data: searchQ });
-  } else {
-    res.status(500).json({ status: 500, error: true, message: 'You have to provide a search' });
+app.post('/movies', async (req, res) => {
+  try {
+    const { title, year, rating } = req.body;
+    const movie = new Movie({ title, year, rating });
+    await movie.save();
+    res.status(201).json({ status: 201, data: movie });
+  } catch (error) {
+    res.status(500).json({ status: 500, error: true, message: error.message });
   }
 });
-const movies = [
-  { title: 'Jaws', year: 1975, rating: 8 },
-  { title: 'Avatar', year: 2009, rating: 7.8 },
-  { title: 'Brazil', year: 1985, rating: 8 },
-  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 },
-];
-app.post('/movies/create', (req, res) => {
-  res.status(200).json({ status: 200, message: 'Movie created' });
-});
 
-app.get('/movies/read', (req, res) => {
-  res.status(200).json({ status: 200, data: movies });
-});
-
-app.put('/movies/update', (req, res) => {
-  res.status(200).json({ status: 200, message: 'Movie updated' });
-});
-
-app.delete('/movies/delete', (req, res) => {
-  res.status(200).json({ status: 200, message: 'Movie deleted' });
-});
-
-app.get('/movies/read/by-date', (req, res) => {
-  const Date = [...movies].sort((a, b) => a.year - b.year);
-  res.status(200).json({ status: 200, data: Date });
-});
-
-app.get('/movies/read/by-rating', (req, res) => {
-  const Rating = [...movies].sort((a, b) => b.rating - a.rating);
-  res.status(200).json({ status: 200, data: Rating });
-});
-
-app.get('/movies/read/by-title', (req, res) => {
-  const Title = [...movies].sort((a, b) => a.title.localeCompare(b.title));
-  res.status(200).json({ status: 200, data: Title });
-});
-
-app.get('/movies/read/:id?',(req,res)=>{
-  const {id} = req.params;
-  if(id > movies.length){
-     res.status(404).json({status:404, error:true, message:`the movie ${id} does not exist`});
-}
-else
-  res.status(200).json({status:200, data:movies[id-1]});
-})
-app.get('/movies/add', (req, res) => {
-  const title = req.query.title;
-  const year = req.query.year;
-  let rating = req.query.rating;
-
-  if (!title || !year || year.length !== 4 || isNaN(year)) {
-    res.status(403).json({
-      status: 403,
-      error: true,
-      message: 'you cannot create a movie without providing a title and a year',
-    });
-  } else {
-    if (!rating || isNaN(rating)) {
-      rating = 4;
-    }
-
-    const newMovie = { title, year: parseInt(year), rating: parseFloat(rating) };
-    movies.push(newMovie);
-
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await Movie.find();
     res.status(200).json({ status: 200, data: movies });
+  } catch (error) {
+    res.status(500).json({ status: 500, error: true, message: error.message });
   }
 });
-let length=movies.length-1;
 
-app.delete('/movies/delete/:id', (req, res) => {
-  const movieId = parseInt(req.params.id);
-  const i = movies.findIndex((movie) => movie.id === movieId);
-
-  if (i!== -1) {
-    movies.splice(i, 1);
-    res.status(200).json({ status: 200, data: movies });
-  } else {
-    res.status(404).json({
-      status: 404,
-      error: true,
-      message: `The movie with ID ${movieId} does not exist`,
-    });
-  }
-});
-app.put('/movies/update/:id', (req, res) => {
-  const movieId = parseInt(req.params.id);
-  const { title: newTitle, rating: newRating } = req.query;
-
-  const movieIndex = movies.findIndex((movie) => movie.id === movieId);
-
-  if (movieIndex !== -1) {
-    const movie = movies[movieIndex];
-
-    if (newTitle) {
-      movie.title = newTitle;
+app.get('/movies/:id', async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) {
+      res.status(404).json({
+        status: 404,
+        error: true,
+        message: `The movie with ID ${req.params.id} does not exist`,
+      });
+    } else {
+      res.status(200).json({ status: 200, data: movie });
     }
-
-    if (newRating) {
-      movie.rating = parseFloat(newRating);
-    }
-
-    res.status(200).json({ status: 200, data: movies });
-  } else {
-    res.status(404).json({
-      status: 404,
-      error: true,
-      message: `The movie with ID ${movieId} does not exist`,
-    });
+  } catch (error) {
+    res.status(500).json({ status: 500, error: true, message: error.message });
   }
 });
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { title, year, rating } = req.body;
+    const updatedMovie = {};
+    if (title) updatedMovie.title = title;
+    if (year) updatedMovie.year = year;
+    if (rating) updatedMovie.rating = rating;
+
+    const movie = await Movie.findByIdAndUpdate(req.params.id, updatedMovie, {
+      new: true, 
+    });
+
+    if (!movie) {
+      res.status(404).json({
+        status: 404,
+        error: true,
+        message: `The movie with ID ${req.params.id} does not exist`,
+      });
+    } else {
+      res.status(200).json({ status: 200, data: movie });
+    }
+  } catch (error) {
+    res.status(500).json({ status: 500, error: true, message: error.message });
+  }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+  try {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+    if (!movie) {
+      res.status(404).json({
+        status: 404,
+        error: true,
+        message: `The movie with ID ${req.params.id} does not exist`,
+      });
+    } else {
+      res.status(200).json({ status: 200, data: movie });
+    }
+  } catch (error) {
+    res.status(500).json({ status: 500, error: true, message: error.message });
+  }
+});
+
+app.listen(3000)
